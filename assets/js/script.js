63b2d51d80dd3b2da8c364bf794884c6ee1c7a41
epicasino2 <rayjonathanbadua@gmail.com>
2023-06-14 03:59:46 -0700

    js array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,59 +1,123 @@
 // Markdown JS 
+function markdown() {
+  const markdownListEl = document.querySelector("#markdownList");
 
-const markdownListEl = document.querySelector("#markdownList");
-
-markdownArray.forEach(element => {
-  markdownListEl.innerHTML += `
+  markdownArray.forEach((element) => {
+    markdownListEl.innerHTML += `
   <li><a href='${element.url}' target='_blank'>${element.title}</a></li>`;
-});
+  });
+};
 
 // Commandline JS
+function commandline() {
+  const commandlineListEl = document.querySelector("#commandlineList");
 
-const commandlineListEl = document.querySelector("#commandlineList");
-
-commandlineArray.forEach(element => {
-  commandlineListEl.innerHTML += `
+  commandlineArray.forEach((element) => {
+    commandlineListEl.innerHTML += `
   <li><a href='${element.url}' target='_blank'>${element.title}</a></li>`;
-});
+  });
+};
 
 // HTML JS
 
-const htmlListEl = document.querySelector("#htmlList");
+function html() {
+  const htmlListEl = document.querySelector("#htmlList");
 
-htmlArray.forEach(element => {
-  htmlListEl.innerHTML += `
+  htmlArray.forEach((element) => {
+    htmlListEl.innerHTML += `
   <li><a href='${element.url}' target='_blank'>${element.title}</a></li>`;
-});
+  });
+};
 
 // CSS JS
 
-const cssBasicsListEl = document.querySelector("#css-basics-list");
-const cssResetListEl = document.querySelector('#css-reset-files-list');
-const cssFlexboxListEl = document.querySelector('#css-flexbox-list');
-const advancedCSSListEl = document.querySelector('#advanced-css-list');
-const cssGamesListEl = document.querySelector('#css-games-list');
+function css() {
+  const cssBasicsListEl = document.querySelector("#css-basics-list");
+  const cssResetListEl = document.querySelector("#css-reset-files-list");
+  const cssFlexboxListEl = document.querySelector("#css-flexbox-list");
+  const advancedCSSListEl = document.querySelector("#advanced-css-list");
+  const cssGamesListEl = document.querySelector("#css-games-list");
 
-cssArray.cssBasics.forEach(element => {
-  cssBasicsListEl.innerHTML += `
+  cssArray.cssBasics.forEach((element) => {
+    cssBasicsListEl.innerHTML += `
   <li><a href='${element.url}' target='_blank'>${element.title}</a></li>`;
-});
+  });
 
-cssArray.cssResetFiles.forEach(element => {
-  cssResetListEl.innerHTML += `
+  cssArray.cssResetFiles.forEach((element) => {
+    cssResetListEl.innerHTML += `
   <li><a href='${element.url}' target='_blank'>${element.title}</a></li>`;
-});
+  });
 
-cssArray.cssFlexbox.forEach(element => {
-  cssFlexboxListEl.innerHTML += `
+  cssArray.cssFlexbox.forEach((element) => {
+    cssFlexboxListEl.innerHTML += `
   <li><a href='${element.url}' target='_blank'>${element.title}</a></li>`;
-});
+  });
 
-cssArray.advancedCSS.forEach(element => {
-  advancedCSSListEl.innerHTML += `
+  cssArray.advancedCSS.forEach((element) => {
+    advancedCSSListEl.innerHTML += `
   <li><a href='${element.url}' target='_blank'>${element.title}</a></li>`;
-});
+  });
 
-cssArray.cssGames.forEach(element => {
-  cssGamesListEl.innerHTML += `
+  cssArray.cssGames.forEach((element) => {
+    cssGamesListEl.innerHTML += `
   <li><a href='${element.url}' target='_blank'>${element.title}</a></li>`;
-});
+  });
+}
+
+// JavaScript JS
+function js() {
+  const jsFunctionListEl = document.querySelector("#js-function-list");
+  const jsArrayListEl = document.querySelector("#arrays-list");
+  const jsMathListEl = document.querySelector("#math-list");
+  const jsMethodsListEl = document.querySelector("#methods-list");
+  const jsWIndowListEl = document.querySelector("#window-list");
+  const jsStatementListEl = document.querySelector("#statements-list");
+  const jsOperatorListEl = document.querySelector("#operators-list");
+  const jsDOMListEl = document.querySelector("#dom-list");
+
+  jsArray.jsFunction.forEach((element) => {
+    jsFunctionListEl.innerHTML += `
+  <li><a href='${element.url}' target='_blank'>${element.title}</a></li>`;
+  });
+
+  jsArray.jsArrayList.forEach((element) => {
+    jsArrayListEl.innerHTML += `
+  <li><a href='${element.url}' target='_blank'>${element.title}</a></li>`;
+  });
+
+  jsArray.jsMath.forEach((element) => {
+    jsMathListEl.innerHTML += `
+  <li><a href='${element.url}' target='_blank'>${element.title}</a></li>`;
+  });
+
+  jsArray.jsMethods.forEach((element) => {
+    jsMethodsListEl.innerHTML += `
+  <li><a href='${element.url}' target='_blank'>${element.title}</a></li>`;
+  });
+
+  jsArray.jsWindow.forEach((element) => {
+    jsWIndowListEl.innerHTML += `
+  <li><a href='${element.url}' target='_blank'>${element.title}</a></li>`;
+  });
+
+  jsArray.jsStatement.forEach((element) => {
+    jsStatementListEl.innerHTML += `
+  <li><a href='${element.url}' target='_blank'>${element.title}</a></li>`;
+  });
+
+  jsArray.jsOperators.forEach((element) => {
+    jsOperatorListEl.innerHTML += `
+  <li><a href='${element.url}' target='_blank'>${element.title}</a></li>`;
+  });
+
+  jsArray.jsDOM.forEach((element) => {
+    jsDOMListEl.innerHTML += `
+  <li><a href='${element.url}' target='_blank'>${element.title}</a></li>`;
+  });
+};
+
+js();
+css();
+html();
+commandline();
+markdown();
