906ac17301009093aa9b17a2d9d3480b42991cf2
jpatterson-cYRYJA <jpatterson@instructors.2u.com>
2023-06-15 13:05:07 -0700

    Adding comments

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,3 +1,19 @@
+/**
+ * --------------Two ways to search => located in the handleSearchButtonClick();
+ * 1. The first way below is finding the matching html nodes on the page and giving back a list. This gives access to the html content of those nodes as well as the href link value
+ * 2. The second way is matching the titles in the linkArray.js
+ *      - This gives back a list of matching titles
+ * 
+ * It should be noted that nothing on the page is displayed. Everything is displayed in the console. 
+ * 
+ * ------------------------------ :Other Notes: ---------------------------------------
+ * Might be a good idea to use the getAllTitles() function to ensure the correct node is grabbed and the correct link is being
+ * displayed
+ */
+
+
+
+
 // grab arrays from linkArrays.js and put them into a single array called arrays 
 let arrays = [
     markdownArray,
@@ -49,33 +65,36 @@ const handleSearchButtonClick = (event) => {
     let searchText = searchInput.value.toLowerCase();
     let searchWords = searchText.split(" ");
     console.log(searchWords)
-    // console.log(searchWords)
+
+
+
+
     // grab all direct ul descendants of .card class and then grab any a tag below that
     const pageText = document.querySelectorAll(".card > ul a");
     // here we will store matching nodes
     let nodeSearch = [];
     pageText.forEach(node => {
         const text = node.textContent.toLowerCase();
-       if(searchWords.some(word => word !== "" && text.includes(word))) {
-        // console.log(node)
+        if (searchWords.some(word => word !== "" && text.includes(word))) {
+            // console.log(node)
 
-        nodeSearch.push(node); // gives us access to any html textcontent that match our search
+            nodeSearch.push(node); // gives us access to any html textcontent that match our search
 
-       } else {
-        // console.log("this does not work :(") // do nothing?
-       }
+        } else {
+            // console.log("this does not work :(") // do nothing?
+        }
     })
-    
+
     console.log(nodeSearch) // check the console when you run a search
-    // const allTitles = getAllTitles(arrays) // grabs all titles from the linkArray.js
+    const allTitles = getAllTitles(arrays) // grabs all titles from the linkArray.js
 
 
-    // const matchingTitles = allTitles.filter(title => searchWords.some(word => title.toLowerCase().includes(word.toLowerCase())))
-    // if (matchingTitles.length === 0) {
-    //     console.log("No matches found!")
-    // } else {
-    //     // console.log(matchingTitles) // uncomment to use and see
-    // }
+    const matchingTitles = allTitles.filter(title => searchWords.some(word => title.toLowerCase().includes(word.toLowerCase())))
+    if (matchingTitles.length === 0) {
+        console.log("No matches found!")
+    } else {
+        console.log(matchingTitles) // uncomment to use and see
+    }
 
 }
 
