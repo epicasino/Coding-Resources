96fd632a70c9cf6331ff63f86b54b6bbd5b7fd34
jpatterson-cYRYJA <jpatterson@instructors.2u.com>
2023-06-15 13:15:38 -0700

    Adding more and more comments

--- assets/js/search.js
+++ assets/js/search.js
@@ -12,8 +12,6 @@
  */
 
 
-
-
 // grab arrays from linkArrays.js and put them into a single array called arrays 
 let arrays = [
     markdownArray,
@@ -42,6 +40,7 @@ const getAllTitles = (arrays) => {
                     titles.push(item.title);
                 }
             });
+            // if the item is an object, it will then go in and check the 'values' of the object and see if any of those has a title -> if they do it will push the title into the titles array located on line 35
         } else if (typeof array === "object") {
             Object.values(array).forEach((subArray) => {
                 subArray.forEach((item) => {
@@ -52,6 +51,7 @@ const getAllTitles = (arrays) => {
             });
         }
     });
+    // return the now full titles array
     return titles;
 }
 
@@ -60,10 +60,14 @@ const getAllTitles = (arrays) => {
 const searchForm = document.getElementById("search");
 let searchInput = document.getElementById("search-input");
 
+// main function
 const handleSearchButtonClick = (event) => {
     event.preventDefault();
+    // lower case the user input
     let searchText = searchInput.value.toLowerCase();
+    // split up words from user input so they can type multiple words and get all corresponding links back
     let searchWords = searchText.split(" ");
+    // console.log our array of searchWords
     console.log(searchWords)
 
 
@@ -77,32 +81,35 @@ const handleSearchButtonClick = (event) => {
         const text = node.textContent.toLowerCase();
         if (searchWords.some(word => word !== "" && text.includes(word))) {
             // console.log(node)
-
+            // push all matching textcontent nodes into our nodeSearch array
             nodeSearch.push(node); // gives us access to any html textcontent that match our search
 
         } else {
-            // console.log("this does not work :(") // do nothing?
+            // console.log("this does not work :(") // do nothing? -- not sure if this else is even needed
         }
     })
 
     console.log(nodeSearch) // check the console when you run a search
+
+    // second method --------------- again this might not even be needed since we can just grab the a nodes that match user input
     const allTitles = getAllTitles(arrays) // grabs all titles from the linkArray.js
 
+    /* this will grab allTitles and filter by title
+    then it will grab our searchWords and run the some() javascript method
+    then in the some() method it will look at each searchWord (assigned to word)
+    then it will return TRUE if title from allTitles includes the word from searchWords
+    then if it returns true, the filter method will put that title into an array called matchingTitles
 
+    */
     const matchingTitles = allTitles.filter(title => searchWords.some(word => title.toLowerCase().includes(word.toLowerCase())))
+    // if not matches 
     if (matchingTitles.length === 0) {
         console.log("No matches found!")
+        // else if matches
     } else {
         console.log(matchingTitles) // uncomment to use and see
     }
 
-}
-
-
-// Placeholder onSearch function
-function onSearch(searchText) {
-    // Perform the search logic here
-    console.log('Search text:', searchText);
 }
 
 
@@ -115,3 +122,12 @@ searchInput.addEventListener("keydown", (event) => {
         handleSearchButtonClick();
     }
 });
+
+
+
+/**
+ * --------------------ISSUES------------------------
+ * 1. One main issue is if the user types in and searches 'the' it will return a matching element that has the values of the so 
+ * for example 'check whether a checkbox is checked in JS' will be returned because the word 'whether' becasue wheTHEr lol 
+ * but main search functionality is there and can be worked on 
+ */
