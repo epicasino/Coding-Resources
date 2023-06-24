68369c9655f052a10d6ca066c6ee26a9ca087cb9
Ray Badua <rayjonathanbadua@gmail.com>
2023-06-24 12:17:05 -0700

    starter code

---
+++ assets/js/collapseButton.js
@@ -0,0 +1,10 @@
+const openDropdownsBtn = document.getElementById("open-dropdowns");
+
+
+const openAllDropdowns = () => {
+  const collapseEls = document.querySelectorAll('main > .c')
+}
+
+
+
+openDropdownsBtn.addEventListener('click', openAllDropdowns);
